143e2048dfa71f2ac9b5903d992f23beda9ca690
Mozaffar <FERAWWWESS@gmail.com>
2024-03-23 22:40:18 +0300

    finish border spacing

--- src/api/useGet.ts
+++ src/api/useGet.ts
@@ -18,6 +18,7 @@ const useGet = (endPoint: any, isObject?: any) => {
     const exam_UUID = isObject?.isExam_UUID ? `&exam_uuid=${isObject?.exam_UUID}` : '';
     const degree = isObject?.isDegree ? `&degree=${isObject?.degree}` : '';
     const position = isObject?.isPosition ? `&position=${isObject?.position}` : '';
+    const basePath = window.location.pathname.split('/').slice(0, -1).join('/');
 
     /* Get data */
     const getData = () => {
@@ -43,7 +44,7 @@ const useGet = (endPoint: any, isObject?: any) => {
                     if (location.pathname.includes("/dashboard")) {
                         navigate('/dashboard/login')
                     } else {
-                        if (location.pathname !== '/' && location.pathname !== '/register') {
+                        if (location.pathname !== basePath + '/' && location.pathname !== `${basePath}/register`) {
                             navigate('/')
                         }
                     }
@@ -56,16 +57,15 @@ const useGet = (endPoint: any, isObject?: any) => {
     }
     /* Check token and get data from initial*/
     React.useEffect(() => {
-        const basePath = window.location.pathname.split('/').slice(0, -1).join('/');
         if (!token) {
             if (location.pathname.includes(`/dashboard`)) {
-                navigate(`${basePath}/dashboard/login`);
-                Cookies.remove("token");
+                navigate('/dashboard/login');
+
             } else {
                 if (
-                    (location.pathname !== basePath && location.pathname !== `${basePath}/register`)
+                    (location.pathname !== basePath + '/' && location.pathname !== `${basePath}/register`)
                 ) {
-                    navigate(basePath);
+                    navigate('/');
                 }
             }
         }
